c19259abe4969aba36fc97f7b480aeebcde91804
octopus <912554887@qq.com>
2026-06-17 15:14:57 +0800

    refactor: move EIP-2929 and precompile support to lib-evm-standard
    Step 4 of ADR-0001. Eip2929AccessState, CheckpointGuard, TransactionPrewarm,
    PrecompileWarm, Util, ModexpGas, PrecompiledAddress, EthTxGasSettlement
    moved to lib-evm-standard package.

---
+++ lib-evm-standard/include/bcos-evm-standard/eip2929/Eip2929Util.h
@@ -0,0 +1,43 @@
+/*
+ *  Copyright (C) 2024 FISCO BCOS.
+ *  SPDX-License-Identifier: Apache-2.0
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ * @brief EIP-2929 feature gating helpers (revision + feature flag).
+ * @file Eip2929Util.h
+ */
+
+#pragma once
+
+#include "transaction-executor/bcos-transaction-executor/vm/RevisionConfig.h"
+#include <evmc/evmc.h>
+
+namespace bcos::executor
+{
+
+struct Eip2929AccessState;
+
+/// True when Berlin+ revision semantics apply and EIP-2929 is enabled in revision config.
+inline bool eip2929Enabled(const bcos::evm_standard::RevisionConfig& rev) noexcept
+{
+    return rev.eip2929;
+}
+
+/// Top-level transaction entry (W1/W2) prewarm applies only at CALL depth 0.
+inline bool eip2929TransactionEntryWarmEnabled(int64_t level,
+    const bcos::evm_standard::RevisionConfig& rev, Eip2929AccessState const* state) noexcept
+{
+    return level == 0 && state != nullptr && eip2929Enabled(rev);
+}
+
+}  // namespace bcos::executor
---
+++ lib-evm-standard/include/bcos-evm-standard/precompiled/ModexpGas.h
@@ -0,0 +1,54 @@
+/*
+ *  Copyright (C) 2024 FISCO BCOS.
+ *  SPDX-License-Identifier: Apache-2.0
+ *  @brief Modexp (0x05) precompile gas and EIP-7823 validation.
+ *  @file ModexpGas.h
+ */
+#pragma once
+
+#include "bcos-utilities/Common.h"
+#include <evmc/evmc.h>
+#include <cstddef>
+#include <string_view>
+
+namespace bcos::evm_standard
+{
+struct RevisionConfig;
+}  // namespace bcos::evm_standard
+
+namespace bcos::executor
+{
+
+struct ModexpLengths
+{
+    bool overflow = false;
+    size_t baseLen = 0;
+    size_t expLen = 0;
+    size_t modLen = 0;
+};
+
+constexpr size_t MODEXP_MAX_FIELD_LEN_EIP7823 = 1024;
+
+ModexpLengths parseModexpLengths(bcos::bytesConstRef input);
+
+inline bool modexpEip7823Enabled(const bcos::evm_standard::RevisionConfig& rev) noexcept
+{
+    return rev.eip7823;
+}
+
+bool validateModexpEip7823(bcos::bytesConstRef input, evmc_revision revision);
+
+bool shouldRejectModexpEip7823(evmc_address const& addr, bcos::bytesConstRef input,
+    const bcos::evm_standard::RevisionConfig& rev, evmc_revision revision) noexcept;
+
+bool shouldRejectModexpEip7823(std::string_view addr, bcos::bytesConstRef input,
+    const bcos::evm_standard::RevisionConfig& rev, evmc_revision revision) noexcept;
+
+/// EIP-198 (< Berlin), EIP-2565 (Berlin..Osaka-1), EIP-7883 (Osaka+).
+bcos::bigint calcModexpGas(bcos::bytesConstRef input, evmc_revision revision);
+
+/// Legacy EIP-198 pricing for PrecompiledRegistrar::pricer("modexp") and unit tests only.
+/// Production uses PrecompiledContract::modexp() -> calcModexpGas(input, revision).
+bcos::bigint calcModexpGasEip198Public(bcos::bytesConstRef input);
+
+}  // namespace bcos::executor
