075e26a9209f541d0d57a68c04b12b8d131f7387
octopus <912554887@qq.com>
2026-06-22 16:23:49 +0800

    feat(eth): add shared EIP-1559 gas formula helpers
    Header-only Eip1559.h with isEip1559GasCapsTx, resolveEffectiveGasPrice,
    tipPerGas, normalizeGasCaps, maxBalanceGasDebit and unit tests.

---
+++ bcos-evm/eth/gas/Eip1559.h
@@ -0,0 +1,79 @@
+/*
+ *  Copyright (C) 2021 FISCO BCOS.
+ *  SPDX-License-Identifier: Apache-2.0
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ * @brief EIP-1559 effective gas price and balance debit helpers.
+ * @file Eip1559.h
+ */
+
+#pragma once
+
+#include "bcos-utilities/Common.h"
+
+namespace bcos::evm::gas
+{
+
+inline bool isEip1559GasCapsTx(uint8_t web3TypedTxKind, bool hasExplicitFeeCapsFromTx) noexcept
+{
+    if (web3TypedTxKind == 0x02 || web3TypedTxKind == 0x04)
+    {
+        return true;
+    }
+    return hasExplicitFeeCapsFromTx;
+}
+
+inline bcos::u256 resolveEffectiveGasPrice(
+    bcos::u256 const& gasTipCap, bcos::u256 const& gasFeeCap, bcos::u256 const& baseFee) noexcept
+{
+    auto const tipPlusBase = gasTipCap + baseFee;
+    return gasFeeCap < tipPlusBase ? gasFeeCap : tipPlusBase;
+}
+
+inline bcos::u256 tipPerGas(bcos::u256 const& effectiveGasPrice, bcos::u256 const& baseFee) noexcept
+{
+    return effectiveGasPrice > baseFee ? effectiveGasPrice - baseFee : bcos::u256{0};
+}
+
+struct GasCaps
+{
+    bcos::u256 gasTipCap;
+    bcos::u256 gasFeeCap;
+    bool isEip1559Caps{false};
+};
+
+inline GasCaps normalizeGasCaps(bcos::u256 gasPrice, bcos::u256 gasTipCap, bcos::u256 gasFeeCap,
+    uint8_t web3TypedTxKind, bool hasExplicitFeeCapsFromTx) noexcept
+{
+    GasCaps caps{};
+    caps.isEip1559Caps = isEip1559GasCapsTx(web3TypedTxKind, hasExplicitFeeCapsFromTx);
+    if (caps.isEip1559Caps)
+    {
+        caps.gasTipCap = gasTipCap;
+        caps.gasFeeCap = gasFeeCap;
+    }
+    else
+    {
+        caps.gasTipCap = gasPrice;
+        caps.gasFeeCap = gasPrice;
+    }
+    return caps;
+}
+
+inline bcos::u256 maxBalanceGasDebit(int64_t gasLimit, GasCaps const& caps) noexcept
+{
+    auto const price = caps.isEip1559Caps ? caps.gasFeeCap : caps.gasTipCap;
+    return bcos::u256(gasLimit) * price;
+}
+
+}  // namespace bcos::evm::gas
---
+++ bcos-evm/test/eth/EthEip1559GasTest.cpp
@@ -0,0 +1,71 @@
+#define BOOST_TEST_MODULE EthEip1559GasTest
+#include "bcos-evm/eth/gas/Eip1559.h"
+#include <boost/test/included/unit_test.hpp>
+
+namespace bcos::evm::test
+{
+using bcos::evm::gas::isEip1559GasCapsTx;
+using bcos::evm::gas::maxBalanceGasDebit;
+using bcos::evm::gas::normalizeGasCaps;
+using bcos::evm::gas::resolveEffectiveGasPrice;
+using bcos::evm::gas::tipPerGas;
+
+BOOST_AUTO_TEST_SUITE(EthEip1559GasTest)
+
+BOOST_AUTO_TEST_CASE(tip_plus_base_below_fee_cap)
+{
+    auto const eff = resolveEffectiveGasPrice(2, 100, 10);
+    BOOST_CHECK_EQUAL(eff, 12);
+}
+
+BOOST_AUTO_TEST_CASE(tip_plus_base_above_fee_cap)
+{
+    auto const eff = resolveEffectiveGasPrice(50, 40, 10);
+    BOOST_CHECK_EQUAL(eff, 40);
+}
+
+BOOST_AUTO_TEST_CASE(legacy_type0_not_1559)
+{
+    BOOST_CHECK(!isEip1559GasCapsTx(0, false));
+    auto const caps = normalizeGasCaps(7, 7, 7, 0, false);
+    BOOST_CHECK(!caps.isEip1559Caps);
+    BOOST_CHECK_EQUAL(caps.gasTipCap, 7);
+    BOOST_CHECK_EQUAL(resolveEffectiveGasPrice(caps.gasTipCap, caps.gasFeeCap, 10), 7);
+}
+
+BOOST_AUTO_TEST_CASE(type1_not_1559)
+{
+    BOOST_CHECK(!isEip1559GasCapsTx(0x01, false));
+}
+
+BOOST_AUTO_TEST_CASE(type2_zero_priority_fee)
+{
+    auto const eff = resolveEffectiveGasPrice(0, 100, 10);
+    BOOST_CHECK_EQUAL(eff, 10);
+}
+
+BOOST_AUTO_TEST_CASE(type4_is_1559)
+{
+    BOOST_CHECK(isEip1559GasCapsTx(0x04, false));
+}
+
+BOOST_AUTO_TEST_CASE(max_balance_debit_1559)
+{
+    gas::GasCaps caps{.gasTipCap = 2, .gasFeeCap = 100, .isEip1559Caps = true};
+    BOOST_CHECK_EQUAL(maxBalanceGasDebit(21'000, caps), u256(21'000) * 100);
+}
+
+BOOST_AUTO_TEST_CASE(max_balance_debit_legacy)
+{
+    gas::GasCaps caps{.gasTipCap = 7, .gasFeeCap = 7, .isEip1559Caps = false};
+    BOOST_CHECK_EQUAL(maxBalanceGasDebit(21'000, caps), u256(21'000) * 7);
+}
+
+BOOST_AUTO_TEST_CASE(tip_per_gas_clamps_at_zero)
+{
+    BOOST_CHECK_EQUAL(tipPerGas(10, 10), 0);
+    BOOST_CHECK_EQUAL(tipPerGas(15, 10), 5);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+}  // namespace bcos::evm::test
