834216c4654e8218259c5623a759ed1c356c8726
octopus <912554887@qq.com>
2026-06-23 15:41:19 +0800

    feat(bcos-evm): add ChainPrecompilePort and AuthPort interfaces

---
+++ bcos-evm/bcos/ports/AuthPort.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "bcos-evm/eth/EVMCResult.h"
+#include <evmc/evmc.h>
+#include <optional>
+#include <string_view>
+
+namespace bcos::evm
+{
+
+struct AuthPort
+{
+    virtual ~AuthPort() = default;
+    virtual std::optional<EVMCResult> checkAuth(evmc_message const& msg) = 0;
+    virtual void createAuthTable(evmc_message const& msg, std::string_view tablePath) = 0;
+};
+
+}  // namespace bcos::evm
